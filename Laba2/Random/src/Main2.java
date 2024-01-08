11813a97912704b12a656b12908e6d8dab3ec04c
Anton1061 <viku0312@mail.ru>
2024-01-08 19:35:21 +0500

    Реализовал создание одномерного массива с 9ью элементами

---
+++ Laba2/Random/src/Main2.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+      public class Main2 {
+	      public static void main(String[] args)
+	      {
+	    	Random rand = new Random();
+	    	
+	    	int mas[] = new int[9];
+	    	
+	    	for(int i = 0; i < 9; i++)
+	    	{
+	    		mas[i] = rand.nextInt(20);
+	    		System.out.println(mas[i]);
+	    	}
+	      }
+	}
+
