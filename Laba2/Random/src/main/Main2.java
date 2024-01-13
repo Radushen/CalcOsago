3a68aa81442aab62939c01dd1a4ff31c5f2aecc5
Radushen <155478062+Radushen@users.noreply.github.com>
2024-01-14 00:43:11 +0500

    Update Main2.java

--- Laba2/Random/src/main/Main2.java
+++ Laba2/Random/src/main/Main2.java
@@ -4,3 +4,14 @@ import java.util.Random;
 public class Main2 {
 public static void main(String[] args)
 {
+Random rand = new Random();
+
+int mas[] = new int[9];
+
+for(int i = 0; i < 9; i++)
+{
+mas[i] = rand.nextInt(20);
+System.out.println(mas[i]);
+}
+}
+}
